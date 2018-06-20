d2c0a1993d51d8a7d1d126ccc0df161ab4569dc2
David Gurley <davidmgurley@gmail.com>
2018-06-19 18:03:41 -0600

    changed indentation

--- version3/src/components/InputForm.js
+++ version3/src/components/InputForm.js
@@ -3,10 +3,10 @@ import React from 'react'
 const InputForm = (props) => {
   return (
     <form id="application-input">
-    <label>Apply Here: </label>
-    <textarea onKeyUp={(event) => {props.onKeyUp(event)}} id="application-text" rows="8" cols="100"></textarea>
-    <input id="submit" type="submit" value="Submit" onClick={(event) => {props.onClick(event)}} />
-    <p id="message">{props.returnMessage}</p>
+      <label>Apply Here: </label>
+      <textarea onKeyUp={(event) => {props.onKeyUp(event)}} id="application-text" rows="8" cols="100"></textarea>
+      <input id="submit" type="submit" value="Submit" onClick={(event) => {props.onClick(event)}} />
+      <p id="message">{props.returnMessage}</p>
     </form>
   )
 }
